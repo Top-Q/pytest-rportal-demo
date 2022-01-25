8762149156fb5a5df7ffd301e99f823d60130105
itaiag <itai.agmon@gmail.com>
2022-01-25 21:01:53 +0200

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,75 @@
+from time import time
+import logging
+import sys
+
+
+import pytest
+
+from pytest_reportportal import RPLogger, RPLogHandler
+
+def timestamp():
+    """Time for difference between start and finish tests."""
+    return str(int(time() * 1000))
+
+@pytest.fixture(scope="session")
+def rp_logger(request):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    # Create handler for Report Portal if the service has been
+    # configured and started.
+    if hasattr(request.node.config, 'py_test_service'):
+        # Import Report Portal logger and handler to the test module.
+        logging.setLoggerClass(RPLogger)
+        rp_handler = RPLogHandler(request.node.config.py_test_service)
+        # Add additional handlers if it is necessary
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.INFO)
+        logger.addHandler(console_handler)
+    else:
+        rp_handler = logging.StreamHandler(sys.stdout)
+    # Set INFO level for Report Portal handler.
+    rp_handler.setLevel(logging.INFO)
+    return logger
+
+@pytest.fixture
+def step(pytestconfig):
+    if not hasattr(pytestconfig, 'py_test_service'):
+        return
+
+    pytest_service = pytestconfig.py_test_service
+    rp_service = pytest_service.rp
+
+    test_item_id = pytest_service.log_item_id
+
+    class Step:
+        def __init__(self, name):
+            self.name = name
+            self.step_id = None
+            self.old_parent_id = None
+
+        def __enter__(self):
+            # Create a 'STEP' item
+            self.step_id = rp_service.start_test_item(name=self.name,
+                                                      start_time=timestamp(),
+                                                      item_type='STEP',
+                                                      has_stats=False,
+                                                      parent_item_id=test_item_id)
+
+            self.old_parent_id = pytest_service.parent_item_id
+
+            pytest_service.log_item_id = self.step_id
+            pytest_service.parent_item_id = self.step_id
+
+        def __exit__(self, type, value, traceback):
+            pytest_service.log_item_id = test_item_id
+            pytest_service.parent_item_id = self.old_parent_id
+            if type:
+                status = 'FAILED'
+            else:
+                status = 'INFO'
+            rp_service.finish_test_item(item_id=self.step_id,
+                                        end_time=timestamp(),
+                                        status=status)
+            rp_service.terminate()
+
+    yield Step
---
+++ tests/core_agent/protocols/test_protocols_connection_steps.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+import pytest
+import logging
+
+log = logging.getLogger('api.endpoint')
+
+class ApiMember:
+
+    def __init__(self):
+        self.email = "foo@bar.com"
+
+@pytest.fixture
+def admin() -> ApiMember:
+    return ApiMember()
+
+
+class ApiNetwork:
+    def __init__(self):
+        self.network = "someNetwork"
+
+    def __str__(self):
+        return self.network
+
+@pytest.fixture
+def network(step) -> ApiNetwork:
+    with step("Given network is set"):
+        log.debug("Doing all kind of things")
+    return ApiNetwork()
+
+
+class Config:
+
+    def switch_protocol_to(self, protocol):
+        log.info(f"Switching protocol to {protocol}")
+        log.debug(
+            "Configuration File",
+            attachment={
+                "name": "agent.conf",
+                "data": "foo=bar\nfoo1=bar1\nfoo2=bar2",
+                "mime": "application/octet-stream",
+            },
+        )
+
+class HostConnection:
+    def __init__(self):
+        self.has_succeeded = True
+
+class Agent:
+
+    def auth(self, tenant, email):
+        log.info(f"Connecting {tenant} with email {email}")
+        log.debug("Request json")
+
+    def resolve_host(self, host):
+        log.info(f'Resolving host {host}')
+        return HostConnection()
+
+    def config(self):
+        return Config()
+
+    def connect(self, network):
+        log.info(f"Connecting to network {network}")
+        log.debug(
+            "Request",
+            attachment={
+                "name": "request.json",
+                "data": """
+        {
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "doggie",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+}""",
+                "mime": "application/json",
+            },
+        )
+
+
+
+@pytest.fixture
+def agent() -> Agent:
+    return Agent()
+
+
+@pytest.mark.component("agent")
+@pytest.mark.os("windows")
+def test_admin_connects_via(step, admin, network, agent):
+    """
+    Given agent is authenticated
+    And the server is configured
+    When the agent connects to the network
+    Then the connection to host perimeter81.com is successful
+    """
+    with step("And agent is authenticated"):
+        agent.auth("tenant", admin.email)
+
+    with step("And the server is configured"):
+        agent.config().switch_protocol_to("http")
+
+    with step("When the agent connects to the network"):
+        agent.connect(network)
+
+    with step("Then the connection to host perimeter81.com is successful"):
+        assert agent.resolve_host('perimeter81.com').has_succeeded
+
+
+@pytest.mark.component("agent")
+@pytest.mark.os("windows")
+def test_admin_failed_to_connect_via(step, admin, network, agent):
+    """
+    Given agent is authenticated
+    And the server is configured
+    When the agent connects to the network
+    Then the connection to host perimeter81.com is successful
+    """
+    with step("Given agent is authenticated"):
+        agent.auth("tenant", admin.email)
+
+    with step("And the server is configured"):
+        agent.config().switch_protocol_to("http")
+
+    with step("When the agent connects to the network"):
+        agent.connect(network)
+
+    with step("Then the connection to host perimeter81.com is successful"):
+        assert not agent.resolve_host('perimeter81.com').has_succeeded
